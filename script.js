0d4d8c971bd740aa57c1a0392bdca790c3ff845a
VaraBhavani <bhavanikandipilli1@gmail.com>
2024-07-10 22:25:26 +0530

    Add files via upload
    to see the code in website https://github.com/VaraBhavani/CodingRaja-Technologies-internship-tasks-resume-builder/upload/main

---
+++ script.js
@@ -0,0 +1,111 @@
+function addNewWEField(){
+  //  console.log("Adding new field");
+
+let newNode =document.createElement("textarea");
+newNode.classList.add("form-control");
+newNode.classList.add("weField");
+newNode.classList.add("mt-2");
+newNode.setAttribute("rows",2);
+newNode.setAttribute("placeholder","Enter here");
+
+let weOb=document.getElementById("we");
+let weAddButtonOb = document.getElementById("weAddButton");
+
+weOb.insertBefore(newNode, weAddButtonOb);
+}
+function addNewAQField(){
+
+let newNode =document.createElement("textarea");
+newNode.classList.add("form-control");
+newNode.classList.add("eqField");
+newNode.classList.add("mt-2");
+newNode.setAttribute("rows",2);
+newNode.setAttribute("placeholder","Enter here");
+
+let aqOb=document.getElementById("aq");
+let aqAddButtonOb = document.getElementById("aqAddButton");
+
+aqOb.insertBefore(newNode, aqAddButtonOb);
+
+}
+function addNewSKField(){
+
+  let newNode =document.createElement("textarea");
+  newNode.classList.add("form-control");
+  newNode.classList.add("skField");
+  newNode.classList.add("mt-2");
+  newNode.setAttribute("rows",2);
+  newNode.setAttribute("placeholder","Enter here");
+  
+  let skOb=document.getElementById("sk");
+  let skAddButtonOb= document.getElementById("skAddButton");
+  
+  skOb.insertBefore(newNode, skAddButtonOb);
+  
+  }
+
+function generateCV()
+{
+ // console.log("generating CV");
+
+
+ let nameField = document.getElementById("nameField").value;
+
+ let nameT1 = document.getElementById("nameT1");
+
+ nameT1.innerHTML =nameField;
+
+ document.getElementById("nameT2").innerHTML = nameField;
+ document.getElementById("contactT").innerHTML = document.getElementById("contactField").value;
+ document.getElementById("addressT").innerHTML = document.getElementById("addressField").value;
+
+ document.getElementById("fbT").innerHTML = document.getElementById("fbField").value;
+ document.getElementById("instaT").innerHTML = document.getElementById("instaField").value;
+ document.getElementById("linkedT").innerHTML = document.getElementById("linkedField").value;
+
+ document.getElementById("objectiveT").innerHTML=document.getElementById("objectiveField").value;
+
+ let wes=document.getElementsByClassName("weField");
+ let str="";
+
+ for(let e of wes)
+ {
+  str=str+`<li> ${e.value} </li>`;
+
+ }
+
+ document.getElementById("weT").innerHTML =str;
+ 
+
+ let sks=document.getElementsByClassName("skField");
+ let str2="";
+
+ for(let e of sks)
+ {
+  str2 += `<li> ${e.value} </li>`;
+
+ }
+
+ document.getElementById("skT").innerHTML =str2;
+ 
+
+
+ let aqs=document.getElementsByClassName("eqField");
+ let str1="";
+
+ for(let e of aqs)
+ {
+  str1 += `<li> ${e.value} </li>`;
+
+ }
+
+ document.getElementById("aqT").innerHTML = str1;
+
+ document.getElementById("cv-form").style.display="none";
+ document.getElementById("cv-template").style.display="block";
+
+ 
+}
+function printCV(){
+  window.print();
+}
